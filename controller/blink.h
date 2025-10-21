3fc98d38518f8985a1363a599449f488f3da1f37
Deanna Earley <dee@earlsoft.co.uk>
2025-10-21 02:48:54 +0100

    Move the blink PIO code into a new module

---
+++ controller/blink.h
@@ -0,0 +1,1 @@
+int blink_init();
--- controller/controller.cpp
+++ controller/controller.cpp
@@ -1,35 +1,20 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "hardware/pio.h"
 #include "hardware/timer.h"
 #include "hardware/clocks.h"
 
 // Modularised functionality
+#include "blink.h"
 #include "i2c.h"
 #include "wifi.h"
 
 // Local pin definitions
 #include "pins.h"
 
-#include "blink.pio.h"
-
-void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq) {
-    blink_program_init(pio, sm, offset, pin);
-    pio_sm_set_enabled(pio, sm, true);
-
-    printf("Blinking pin %d at %d Hz\n", pin, freq);
-
-    // PIO counter program takes 3 more cycles in total than we pass as
-    // input (wait for n + 1; mov; jmp)
-    pio->txf[sm] = (125000000 / (2 * freq)) - 3;
-}
-
 int64_t alarm_callback(alarm_id_t id, void *user_data) {
     // Put your timeout handler code in here
     return 0;
 }
-
-
 
 int main()
 {
@@ -42,16 +27,7 @@ int main()
     i2c_init2(I2C_PORT, I2C_BAUD_RATE, I2C_SDA, I2C_SCL);
 
     // PIO Blinking example
-    PIO pio = pio0;
-    uint offset = pio_add_program(pio, &blink_program);
-    printf("Loaded program at %d\n", offset);
-
-    #ifdef PICO_DEFAULT_LED_PIN
-    blink_pin_forever(pio, 0, offset, PICO_DEFAULT_LED_PIN, 3);
-    #else
-    blink_pin_forever(pio, 0, offset, 6, 3);
-    #endif
-    // For more pio examples see https://github.com/raspberrypi/pico-examples/tree/master/pio
+    blink_init();
 
     // Timer example code - This example fires off the callback after 2000ms
     add_alarm_in_ms(2000, alarm_callback, NULL, false);
